18aa62ab960c0ab968f7df9f03b749049c6a4d7e
Mark Valdez <Mark Valdez>
2016-09-05 21:05:08 -0700

    add invalid indicators and loading gif on report page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,40 +1,14 @@
-import { 
-	Component, 
-	Input,
-	trigger,
-	state,
-	style,
-	transition,
-	animate 
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
   moduleId: module.id,
   selector: 'app-home',
   templateUrl: 'home.component.html',
-  styleUrls: ['home.component.css'],
-  animations: [
-  	trigger('shrinktrigger', [
-  		state('firstpos', style({transform: 'scale(1)'})),
-  		state('secondpos', style({transform: 'scale(0)'})),
-  		transition('firstpos => secondpos', [
-  			animate('1s ease-in')
-  		]),
-  		transition('secondpos => firstpos', [
-  			animate('1s ease-in')
-  		])
-  	])
-  ]
+  styleUrls: ['home.component.css']
 })
 export class HomeComponent {
 
   constructor() { }
-
-  state: string = 'firstpos';
-
-  togglestate() {
-  	this.state = (this.state === 'firstpos' ? 'secondpos' : 'firstpos');
-  }
 
 }
